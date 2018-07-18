f436ed7a9b700b76c7618ef697bed7816f295930
Sydney Bridges <sybrid@gmail.com>
2018-07-17 21:59:38 -0400

    Added console message for clicks

--- scripts/script.js
+++ scripts/script.js
@@ -8,14 +8,22 @@ var ctx = canvas.getContext("2d");
     var numSquaresX = 25;
     var numSquaresY = 25;
 
+    // This loop should draw all the vertical lines
     for(var x = 0; x <= numSquaresX; x++) {
         ctx.moveTo(x * squareSide, 0);
         ctx.lineTo(x * squareSide, numSquaresY * squareSide);
         ctx.stroke();
     }
+    // This loop should draw all the horizontal lines
     for(var y = 0; y <= numSquaresY; y++) {
         ctx.moveTo(0, y * squareSide);
         ctx.lineTo(numSquaresX * squareSide, y * squareSide);
         ctx.stroke();
     }
+
+    document.addEventListener("click", onClick, false);
 })();
+
+var onClick = function(event) {
+    console.log("Click at " + event.x + ", " + event.y);
+}
