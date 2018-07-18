5c4b92aea8245169fd23d19eb1fbba044a4c696e
Sydney Bridges <sybrid@gmail.com>
2018-07-18 05:27:18 -0400

    Added function to get grid coordinates from canvas position

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+// Variables
+var squareSide = 20;
+var numSquaresX = 25;
+var numSquaresY = 25;
+
 //ctx.fillStyle = "#FF0000";
 //ctx.fillRect(0,0,150,75);
 //Draw a grid 30x20 squares
 (function(){
-    var squareSide = 20;
-    var numSquaresX = 25;
-    var numSquaresY = 25;
-
     // This loop should draw all the vertical lines
     for(var x = 0; x <= numSquaresX; x++) {
         ctx.moveTo(x * squareSide, 0);
@@ -27,8 +29,10 @@ var ctx = canvas.getContext("2d");
 
 function onClick (canvas, event) {
     var coords = getMousePosRelativeToCanvas(canvas, event);
+    var gridCoords = getGridCoordFromCanvasPosition(coords);
     console.log("Click at " + event.x + ", " + event.y);
     console.log("That translates to " + coords.toString());
+    console.log("That translates to a grid position of " + gridCoords.toString());
 }
 
 function getMousePosRelativeToCanvas (canvas, event) {
@@ -41,4 +45,29 @@ function getMousePosRelativeToCanvas (canvas, event) {
             return string;
         })
     };
+}
+
+function getGridCoordFromCanvasPosition (position) {
+    var calcx, calcy;
+    calcx = Math.floor(position.x / squareSide);
+    calcy = Math.floor(position.y / squareSide);
+
+    if( calcx < 0 || calcx > numSquaresX || calcy < 0 || calcy > numSquaresY) {
+        return {
+            x: -1,
+            y: -1,
+            toString: (function(){
+                return 'out of bounds';
+            })
+        };
+    } else {
+        return {
+            x: calcx,
+            y: calcy,
+            toString: (function(){
+                var string = this.x + ", " + this.y;
+                return string;
+            })
+        };
+    }
 }
