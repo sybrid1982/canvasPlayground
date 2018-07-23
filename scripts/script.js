6e93a2448eb529ffb119c0742de5f7e840c96d84
Sydney Bridges <sybrid@gmail.com>
2018-07-23 14:36:35 -0400

    Replaced all the var with let, fixed code so it still works.

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,28 @@
 "use strict";
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
 
 let GameSession = function() {
-    // Constant Variables
-    var canvasSize = 500;
-    var squareSide = 50;
-    var numSquaresX = 10;
-    var numSquaresY = 10;
-    var numMines = 15;
-    var directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
-    var flagIMG = setupFlagImage();
-    var blankColor = '#C0C0C0';
+    // Constant letiables
+    let canvasSize = 500;
+    let squareSide = 50;
+    let numSquaresX = 10;
+    let numSquaresY = 10;
+    let numMines = 15;
+    let directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    let flagIMG = setupFlagImage();
+    let blankColor = '#C0C0C0';
 
-    // Calculated Variables
-    var grid, gridSize;
+    // Calculated letiables
+    let grid, gridSize;
 
-    // Variable Variables
-    var revealModeOn = true;
-    var gameOver = false;
-    var flaggedSquares = 0;
+    // letiable letiables
+    let revealModeOn = true;
+    let gameOver = false;
+    let flaggedSquares = 0;
 
     // Constructor for Square objects
-    var Square = function(index, position){
+    let Square = function(index, position){
         this.hasMine = false;
         this.revealed = false;
         this.flagged = false;
@@ -37,7 +37,7 @@ let GameSession = function() {
 
     function setupGrid(){
         // First, get the input from the fields
-        var inSizeX = document.getElementById('gridSize').value;
+        let inSizeX = document.getElementById('gridSize').value;
         // Check if the grid size 
         if(isNaN(inSizeX)) {
             inSizeX = 10;
@@ -49,13 +49,13 @@ let GameSession = function() {
         squareSide = canvasSize / numSquaresX;
 
         // This loop should draw all the vertical lines
-        for(var x = 0; x <= numSquaresX; x++) {
+        for(let x = 0; x <= numSquaresX; x++) {
             ctx.moveTo(x * squareSide, 0);
             ctx.lineTo(x * squareSide, numSquaresY * squareSide);
             ctx.stroke();
         }
         // This loop should draw all the horizontal lines
-        for(var y = 0; y <= numSquaresY; y++) {
+        for(let y = 0; y <= numSquaresY; y++) {
             ctx.moveTo(0, y * squareSide);
             ctx.lineTo(numSquaresX * squareSide, y * squareSide);
             ctx.stroke();
@@ -82,7 +82,7 @@ let GameSession = function() {
         } else {
             numMines = Math.floor(numMines);
         }
-        var minesToPlace = numMines;
+        let minesToPlace = numMines;
 
         if(grid && grid.length > 0) {
             // Destroy the old grid to make sure we don't have a bunch of crap
@@ -92,13 +92,13 @@ let GameSession = function() {
         }
 
         grid = [];
-        for(var index = 0; index < gridSize; index++) {
-            var coords = getCoordsFromIndex(index);
-            var square = new Square(index, coords);
+        for(let index = 0; index < gridSize; index++) {
+            let coords = getCoordsFromIndex(index);
+            let square = new Square(index, coords);
             // Set neighbors for the new square
             // Logic outline:  If the x coord is > 0, then there should be a neighbor to the west
             // That neighbor will be the one at index - 1
-            setNeighbors();
+            setNeighbors(coords, square, index);
 
             grid.push(square);
             colorSquare(coords, blankColor);
@@ -108,30 +108,30 @@ let GameSession = function() {
             minesToPlace = gridSize;
         }
 
-        var loopCount = 0;
+        let loopCount = 0;
         placeMines(minesToPlace);
 
-        function setNeighbors() {
+        function setNeighbors(coords, square, index) {
             if (coords.x > 0) {
-                var wNeighbor = grid[index - 1];
+                let wNeighbor = grid[index - 1];
                 square.setNeighbor(wNeighbor, 'W');
                 wNeighbor.setNeighbor(square, 'E');
                 // If Y is > 0, X and Y > 0 and we have a northwest neighbor
                 if (coords.y > 0) {
                     // That neighbor is at index-gridSizeX
-                    var nwNeighbor = grid[index - numSquaresX - 1];
+                    let nwNeighbor = grid[index - numSquaresX - 1];
                     square.setNeighbor(nwNeighbor, 'NW');
                     nwNeighbor.setNeighbor(square, 'SE');
                 }
             }
             // Set North neighbors if y > 0
             if (coords.y > 0) {
-                var nNeighbor = grid[index - numSquaresX];
+                let nNeighbor = grid[index - numSquaresX];
                 square.setNeighbor(nNeighbor, 'N');
                 nNeighbor.setNeighbor(square, 'S');
                 // If Y > 0, then we may have a NE neighbor, but only if we aren't at gridSizeX index
                 if (coords.x < numSquaresX - 1) {
-                    var neNeighbor = grid[index - numSquaresX + 1];
+                    let neNeighbor = grid[index - numSquaresX + 1];
                     square.setNeighbor(neNeighbor, 'NE');
                     neNeighbor.setNeighbor(square, 'SW');
                 }
@@ -140,9 +140,9 @@ let GameSession = function() {
     }
 
     function placeMines(minesToPlace) {
-        var loopCount = 0;
+        let loopCount = 0;
         while(minesToPlace > 0 && loopCount < 50) {
-            var indexToMine = Math.floor(Math.random()*gridSize);
+            let indexToMine = Math.floor(Math.random()*gridSize);
             if(grid[indexToMine].hasMine === false) {
                 grid[indexToMine].hasMine = true;
                 minesToPlace--;
@@ -154,15 +154,15 @@ let GameSession = function() {
 
     function onClick (canvas, event) {
         if(!gameOver) {
-            var coords = getMousePosRelativeToCanvas(canvas, event);
-            var gridCoords = getGridCoordFromCanvasPosition(coords);
-            var colorClear = '#ffffff';
-            var colorMine = '#ff0000';
+            let coords = getMousePosRelativeToCanvas(canvas, event);
+            let gridCoords = getGridCoordFromCanvasPosition(coords);
+            let colorClear = '#ffffff';
+            let colorMine = '#ff0000';
 
             // First, check if the click is on the grid
             if(gridCoords.x !== -1) {
                 // If we are on the grid, we can get the square
-                var square = getSquareFromCoords(gridCoords);
+                let square = getSquareFromCoords(gridCoords);
                 console.log(`${gridCoords} square revealed is ${square.revealed} and flagged is ${square.flagged}`);
                 directions.forEach(function(direction) {
                     console.log(`${direction} neighbor is ${getCoordsFromIndex(square[direction].index).toString()}`);
@@ -172,7 +172,7 @@ let GameSession = function() {
                     if(!checkForMine(gridCoords)){
                         square.revealed = true;
                         colorSquare(gridCoords, colorClear);
-                        var numMinesAround = checkNeighborsForMines(gridCoords);
+                        let numMinesAround = checkNeighborsForMines(gridCoords);
                         if(numMinesAround > 0) {
                             drawTextAtCoords(numMinesAround, gridCoords);
                         }
@@ -195,19 +195,19 @@ let GameSession = function() {
 
 
     function getMousePosRelativeToCanvas (canvas, event) {
-        var rect = canvas.getBoundingClientRect();
+        let rect = canvas.getBoundingClientRect();
         return {
             x: event.x - rect.left,
             y: event.y - rect.top,
             toString: (function(){
-                var string = this.x + ", " + this.y;
+                let string = this.x + ", " + this.y;
                 return string;
             })
         };
     }
 
     function getGridCoordFromCanvasPosition (position) {
-        var calcx, calcy;
+        let calcx, calcy;
         calcx = Math.floor(position.x / squareSide);
         calcy = Math.floor(position.y / squareSide);
 
@@ -224,7 +224,7 @@ let GameSession = function() {
                 x: calcx,
                 y: calcy,
                 toString: (function(){
-                    var string = this.x + ", " + this.y;
+                    let string = this.x + ", " + this.y;
                     return string;
                 })
             };
@@ -233,8 +233,8 @@ let GameSession = function() {
 
     // Left behind because it's cool, but no longer used
     // function returnRandomColor(gridPosition) {
-    //     var startX, startY, endX, endY;
-    //     var color;
+    //     let startX, startY, endX, endY;
+    //     let color;
     //     if(revealModeOn){
     //         color = getRandomColor();
     //     } else {
@@ -247,26 +247,26 @@ let GameSession = function() {
         ctx.fillStyle = color;
 
         // If we want to leave the lines behind, our x and y for the fill need +1 and -2 for the fill start and end respectively
-        var startX = gridPosition.x * squareSide + 1;
-        var startY = gridPosition.y * squareSide + 1;
-        var endX = squareSide - 2;
-        var endY = squareSide - 2;
+        let startX = gridPosition.x * squareSide + 1;
+        let startY = gridPosition.y * squareSide + 1;
+        let endX = squareSide - 2;
+        let endY = squareSide - 2;
 
         ctx.fillRect(startX, startY, endX, endY); 
     }
 
     // Cribbed from jagadesha on Medium
     function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for(var i = 0; i < 6; i++) {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for(let i = 0; i < 6; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
     }
 
     function toggleRevealMode() {
-        var buttonString;
+        let buttonString;
         revealModeOn = !revealModeOn;
         if(revealModeOn) {
             // set the text of the button to reveal mode
@@ -280,7 +280,7 @@ let GameSession = function() {
     }
 
     function checkForMine(coords) {
-        var index = getIndexFromCoords(coords);
+        let index = getIndexFromCoords(coords);
 
         if(grid[index].hasMine) {
             return true;
@@ -306,13 +306,13 @@ let GameSession = function() {
     // Given a set of coordinates, returns how many mines are around that set of coordinates
     // If it's greater than 0, then a later function will write that number as text in the square
     function checkNeighborsForMines(coords) {
-        var square = getSquareFromCoords(coords);
+        let square = getSquareFromCoords(coords);
 
-        var numberOfNeighborsWithMines = 0;
+        let numberOfNeighborsWithMines = 0;
 
         directions.forEach(function(direction) {
             // console.log(direction);
-            var neighbor = square[direction];
+            let neighbor = square[direction];
             // console.log(`${direction} neighbor is ${neighbor}`);
             if(neighbor !== -1 && neighbor.hasMine) {
                 numberOfNeighborsWithMines++;
@@ -323,8 +323,8 @@ let GameSession = function() {
     }
 
     function getCoordsFromIndex(index) {
-        var coordx = index % numSquaresX;
-        var coordy = Math.floor(index / numSquaresX);
+        let coordx = index % numSquaresX;
+        let coordy = Math.floor(index / numSquaresX);
 
         return {
             x: coordx,
@@ -343,13 +343,13 @@ let GameSession = function() {
      * opposite direction to a passed function */
     
     // function getOppositeDirection(direction) {
-    //     var directionsIndex = directions.indexOf(direction);
+    //     let directionsIndex = directions.indexOf(direction);
     //     console.log(directionsIndex);
     //     if (directionsIndex >= 0 && directionsIndex < 4) {
-    //         var oppositeIndex = directionsIndex + 4;
+    //         let oppositeIndex = directionsIndex + 4;
     //         return directions[oppositeIndex];
     //     } else if (directionsIndex >= 4) {
-    //         var oppositeIndex = directionsIndex - 4;
+    //         let oppositeIndex = directionsIndex - 4;
     //         return directions[oppositeIndex];
     //     } else {
     //         return -1;
@@ -357,7 +357,7 @@ let GameSession = function() {
     // }
 
     function getSquareFromCoords(coords) {
-        var index = getIndexFromCoords(coords);
+        let index = getIndexFromCoords(coords);
         return grid[index];
     }
 
@@ -365,7 +365,7 @@ let GameSession = function() {
         ctx.textAlign='center';
         // We could put a switch in here to make the text a different color for different values of mines
         ctx.font = Math.floor(squareSide / 2) + 'pt Arial';
-        var color = '#000000';
+        let color = '#000000';
         
         // Set the color for the text
         // 1 will be black, 2 blue, 3 yellow, and 4+ red
@@ -384,11 +384,32 @@ let GameSession = function() {
                 break;
         }
 
-        var positionx = coords.x * squareSide + 0.5 * squareSide;
-        var positiony = coords.y * squareSide + 0.75 * squareSide;
+        let positionx = coords.x * squareSide + 0.5 * squareSide;
+        let positiony = coords.y * squareSide + 0.75 * squareSide;
 
         ctx.fillStyle = color;
         ctx.fillText(text, positionx, positiony);
+    }
+
+    // Takes hexcode colors and linearlly interpolates between
+    // them to get a new color.
+    function lerpColor(colorA, colorB, lerpAmount) {
+        let colorAClean = colorCleaner(colorA);
+        let colorBClean = colorCleaner(colorB);
+
+
+    }
+
+    function colorCleaner(color) {
+        let colorInternal = color;
+        if(color[0] = '#') {
+            let temp = '';
+            for(let i = 1; i <= 7; i++) {
+                temp += color[i];
+            }
+            colorInternal = temp;
+        }
+        return colorInternal;
     }
 
     function startNewGame() {
@@ -399,14 +420,14 @@ let GameSession = function() {
     // FLAG DRAWING FUNCTIONS
     // Load the flag from the source image
     function setupFlagImage() {
-        var flag_image = new Image();
+        let flag_image = new Image();
         flag_image.src = "../Images/simple-flag-two-color.jpg";
         return flag_image;
     }
     // Draw the flag at game board coordinates
     function drawFlag(coords){
-        var positionx = coords.x * squareSide + 0.1 * squareSide;
-        var positiony = coords.y * squareSide + 0.1 * squareSide;
+        let positionx = coords.x * squareSide + 0.1 * squareSide;
+        let positiony = coords.y * squareSide + 0.1 * squareSide;
         console.log("DrawFlag called");
         ctx.drawImage(flagIMG, positionx, positiony, squareSide * .8, squareSide * .8);
     }
